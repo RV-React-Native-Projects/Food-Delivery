1038153c42cbbb7110615a7020f10e32c05f5315
Ranvijay Kumar <ranvijaychouhan12@gmail.com>
2021-05-30 17:30:34 +0530

    First Commit
    Complete Project

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Home, OrderDelivery, Restaurant } from './Screens';
+import Tabs from './Navigation/Tabs';
+
+const Stack = createStackNavigator();
+
+// import { Font } from 'expo';
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	// useEffect(() => {
+	// 	Font.loadAsync({
+	// 		Roboto: require('./assets/fonts/Roboto-Regular.ttf')
+	// 	});
+	// }, []);
+
+	return (
+		<SafeAreaProvider>
+			<NavigationContainer>
+				<Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Tabs">
+					<Stack.Screen name="Tabs" component={Tabs} />
+					<Stack.Screen name="Home" component={Home} />
+					<Stack.Screen name="OrderDelivery" component={OrderDelivery} />
+					<Stack.Screen name="Restaurant" component={Restaurant} />
+				</Stack.Navigator>
+			</NavigationContainer>
+		</SafeAreaProvider>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
 });
---
+++ README.md
@@ -0,0 +1,45 @@
+# Food DELIVERY APP using React-Native ( EXPO )
+
+
+## Table of Contents
+
+| Code | Project | Preview | Inspiration | No. of Screens |
+| ------ | ------ | ------ | ------ | ------ |
+| LCRN08 |
+ [Food Delivery App]| 
+
+
+ ## SCREEN SHOTS
+
+
+ <img src="./Screen-Shots/1.jpg" width="300" /> 
+ <img src="./Screen-Shots/2.jpg" width="300" /> 
+ <img src="./Screen-Shots/3.jpg" width="300" /> 
+ <img src="./Screen-Shots/4.jpg" width="300" /> 
+ <img src="./Screen-Shots/5.jpg" width="300" /> 
+ <img src="./Screen-Shots/6.jpg" width="300" /> 
+ <img src="./Screen-Shots/7.jpg" width="300" /> 
+ <img src="./Screen-Shots/8.jpg" width="300" /> 
+ <img src="./Screen-Shots/9.jpg" width="300" /> 
+ <img src="./Screen-Shots/10.jpg" width="300" /> 
+ <img src="./Screen-Shots/11.jpg" width="300" /> 
+ <img src="./Screen-Shots/12.jpg" width="300" /> 
+ <img src="./Screen-Shots/13.jpg" width="300" /> 
+ <img src="./Screen-Shots/14.jpg" width="300" /> 
+ <img src="./Screen-Shots/15.jpg" width="300" /> 
+ 
+ 
+ 
+ 
+ 
+ | 
+
+
+<!-- 
+ [View](https://dribbble.com/shots/14527824-Food-Delivery-Mobile-App) | 3 | -->
+
+## Contributors
+
+<a href="https://github.com/RV-React-Native-Projects/Food-Delivery">
+  <img src="https://avatars.githubusercontent.com/u/67736054?s=96&v=4" />
+</a>
